7e9f1dc54a195441484496c453d0ddb7a2fc9c0f
ned <nedschultz@outlook.com>
2022-12-05 16:52:45 +1000

    commit tree 1d96fda51ea1a2a8726d32cf44ee13e0e82f5e93

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+
+    let messages = [];
+
+    for (let index = 0; index < names.length; index++) {
+
+        messages.push(
+            `Thank you, ${names[index]}, for the wonderful surprise gift!`
+            );
+    }
+    return messages;
+} 
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num--);
+    }
+}
